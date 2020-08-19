2910a36ff3616110a1f805e7afa1cbffdda9e16b
fanpengfei1 <fanpengfei@jd.com>
2020-08-19 19:02:16 +0800

    fw

--- examples/echo.cc
+++ examples/echo.cc
@@ -1,11 +1,23 @@
+
+#include <iostream>
 #include <handy/handy.h>
+
 using namespace handy;
+using namespace std;
 
 int main(int argc, const char *argv[]) {
     EventBase base;
     Signal::signal(SIGINT, [&] { base.exit(); });
     TcpServerPtr svr = TcpServer::startServer(&base, "", 2099);
     exitif(svr == NULL, "start tcp server failed");
-    svr->onConnRead([](const TcpConnPtr &con) { con->send(con->getInput()); });
+    svr->onConnRead(
+        [](const TcpConnPtr &con) { 
+
+        Slice s = con->getInput();
+
+        std::cout << "con->getInput:" << s.toString() << std::endl;
+        con->send(con->getInput()); 
+        }
+        );
     base.loop();
 }
