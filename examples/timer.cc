c96dc9f9eaae8fe9a1e4add49c809e22e73e38fd
ebayboy <ebayboy@163.com>
2020-08-26 15:30:23 +0800

    fw

--- examples/timer.cc
+++ examples/timer.cc
@@ -13,17 +13,20 @@ int main(int argc, const char *argv[]) {
 
     info("program begin");
 
+    //case1
     base.runAfter(200, []() { info("a task in runAfter 200ms"); });
 
+    //case2
     base.runAfter(100, []() { info("a task in runAfter 100ms interval 1000ms"); }, 1000);
 
+    //case3
     TimerId id = base.runAt(time(NULL) * 1000 + 300, []() { info("a task in runAt now+300 interval 500ms"); }, 500);
-
     base.runAfter(2000, [&]() {
         info("cancel task of interval 500ms");
         base.cancel(id);
     });
 
+    //case4
     //runafter 3000ms, then base.exit()
     base.runAfter(3000, [&]() { base.exit(); });
 
